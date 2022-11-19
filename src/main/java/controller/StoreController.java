2a01d33dd1d183a7038ebda97bc9934ba935a61e
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-11-19 18:42:09 +0300

    сделала ... но ничего не работает (

--- src/main/java/controller/StoreController.java
+++ src/main/java/controller/StoreController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.Item;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.StoreService;
+
+import java.util.List;
+
+@RestController
 public class StoreController {
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping("/store/order/get")
+    public List<Item> getAllItems() {
+        return this.storeService.getAllItems();
+    }
+
+    @GetMapping("/store/order/add")
+    public List addItems(@RequestParam ("id") List <Integer> id){
+        return this.storeService.addItem(id);
+    }
+
+
+
 }
--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Item {
+    private final int id;
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                '}';
+    }
 }

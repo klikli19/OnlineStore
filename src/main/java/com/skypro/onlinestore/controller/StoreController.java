38bc28703e0f04808e5753b66c8ae4c954b00c62
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-11-19 20:07:57 +0300

    поправочка

--- src/main/java/com/skypro/onlinestore/controller/StoreController.java
+++ src/main/java/com/skypro/onlinestore/controller/StoreController.java
@@ -3,6 +3,7 @@ package com.skypro.onlinestore.controller;
 import com.skypro.onlinestore.model.Item;
 import com.skypro.onlinestore.service.StoreService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/order")
 public class StoreController {
     private final StoreService storeService;
 
@@ -17,12 +19,12 @@ public class StoreController {
         this.storeService = storeService;
     }
 
-    @GetMapping("/order/get")
+    @GetMapping("/get")
     public List<Item> getAllItems() {
         return storeService.getAllItems();
     }
 
-    @GetMapping("/order/add")
+    @GetMapping("/add")
     public List addItems(@RequestParam ("id") List <Integer> id){
         return storeService.addItem(id);
     }
